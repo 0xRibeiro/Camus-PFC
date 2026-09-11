61a32ad7dbeea68ba6408534fa57638af06adc59
Victor Ribeiro <142441056+0xRibeiro@users.noreply.github.com>
2026-09-11 13:46:46 +0000

    feat: Adiciona a tabela associativa entre usuários e conquistas.

--- backend/app/domains/conquistas/model.py
+++ backend/app/domains/conquistas/model.py
@@ -1,7 +1,8 @@
-from sqlalchemy import String, Text
+from sqlalchemy import Boolean, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base
+from app.domains.usuario import usuario
 
 
 # inicialmente pensei nesses atributos para a conquista. a tabela
@@ -17,3 +18,29 @@ class Conquista(Base):
 
 	# guarda o link da imagem do achievement
 	imagem_dir: Mapped[str] = mapped_column(Text)
+
+
+# Tabela associativa para vincular usuário com conquista.
+# feita no ambiente virtual github, conferir se funcionou
+# no devenv assim que possivel.
+class ConquistaAdquiria(Base):
+	__tablename__ = 'conquista_adquirida'
+    id = Column(Integer, primary_key=True, index=True)
+    conquistaId = Column(Integer, ForeignKey('Item.id'))
+    usuarioId = Column(Integer, ForeignKey('Usuario.id'))
+	desbloqueada = Column(Boolean)
+
+
+class ItemDetail(Base):
+    __tablename__ = 'ItemDetail'
+    id = Column(Integer, primary_key=True, index=True)
+    itemId = Column(Integer, ForeignKey('Item.id'))
+    detailId = Column(Integer, ForeignKey('Detail.id'))
+    endDate = Column(Date)
+
+class ConquistaAdquirida(Base):
+	__tablename__= "conquista_adquirida"
+
+	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+	id_usuario[int] = mapped_column(fo)
