19116e98d0053d85e6f128ae1a4ce354ae50a337
Victor <142441056+0xRibeiro@users.noreply.github.com>
2026-09-12 18:03:02 -0300

    chore: :construction: atualiza o schema das conquistas para incluir as classes da tabela de associação

--- backend/app/domains/conquistas/schema.py
+++ backend/app/domains/conquistas/schema.py
@@ -19,3 +19,22 @@ class ConquistaUpdate(BaseModel):
 	titulo: str | None = None
 	descricao: str | None = None
 	desbloquada: bool | None = None
+
+
+# abaixo são os schemas da tabela associativa. não tenho certeza se é necessário
+# ou recomendado ter todos esses metodos pra ela, mas manterei por enquanto e deleterei
+# caso seja necessario.
+
+class associacao_conquista_usuarioCreate(BaseModel):
+	usuario_id: int
+	conquista_id: int
+
+class associacao_conquista_usuarioRead(BaseModel):
+	model_config = ConfigDict(from_attributes=True)
+
+	usuario_id: int
+	conquista_id: int
+
+class associacao_conquista_usuarioUpdate(BaseModel):
+	usuario_id: int | None = None
+	conquista_id: int | None = None
